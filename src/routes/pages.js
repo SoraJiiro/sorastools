2f2af37fe450fe467d612df267c738d918578e61
Sora <100049475+SoraJiiro@users.noreply.github.com>
2026-06-26 15:42:19 +0200

    Move page routes to dedicated file

---
+++ src/routes/pages.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const { PUBLIC_DIR } = require("../config/paths");
+
+const router = express.Router();
+
+function sendPublicFile(res, ...segments) {
+  res.sendFile(path.join(PUBLIC_DIR, ...segments));
+}
+
+router.get("/", (req, res) => {
+  sendPublicFile(res, "index.html");
+});
+
+router.get("/tools/color-picker", (req, res) => {
+  sendPublicFile(res, "tools", "color-picker.html");
+});
+
+router.get("/tools/json-formatter", (req, res) => {
+  sendPublicFile(res, "tools", "json-formatter.html");
+});
+
+router.get("/tools/regex-tester", (req, res) => {
+  sendPublicFile(res, "tools", "regex-tester.html");
+});
+
+router.get("/tools/md-previewer", (req, res) => {
+  sendPublicFile(res, "tools", "md-previewer.html");
+});
+
+router.use((req, res) => {
+  res.status(404);
+  sendPublicFile(res, "404.html");
+});
+
+module.exports = router;
