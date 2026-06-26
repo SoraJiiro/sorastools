6e37c7c87e44d7534430bee1f8631445beecaf38
Sora <100049475+SoraJiiro@users.noreply.github.com>
2026-06-26 17:36:40 +0200

    Use data-label selector for action labels

--- public/js/global.js
+++ public/js/global.js
@@ -1,4 +1,4 @@
 import { applyActionsLabels, setupTextareaTabHandlers } from "./utils.js";
 
 setupTextareaTabHandlers("textarea:not([readonly])");
-applyActionsLabels("button, a, input, label, select, textarea");
+applyActionsLabels();
