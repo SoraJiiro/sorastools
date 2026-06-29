1124af36b96c641dd0e9bfa02acf543a02f7f4d5
Sora <100049475+SoraJiiro@users.noreply.github.com>
2026-06-29 17:37:40 +0200

    Expose node_modules path config

--- src/config/paths.js
+++ src/config/paths.js
@@ -3,11 +3,13 @@ const path = require("path");
 const ROOT_DIR = path.join(__dirname, "..", "..");
 const SRC_DIR = path.join(ROOT_DIR, "src");
 const PUBLIC_DIR = path.join(ROOT_DIR, "public");
+const NODE_MODULES_DIR = path.join(ROOT_DIR, "node_modules");
 const TOOLS_FILE = path.join(SRC_DIR, "data", "toolStorage.json");
 
 module.exports = {
   ROOT_DIR,
   SRC_DIR,
   PUBLIC_DIR,
+  NODE_MODULES_DIR,
   TOOLS_FILE,
 };
