f1be1df660e4da49aa9bc1f28ee09035469475e2
Sora <100049475+SoraJiiro@users.noreply.github.com>
2026-06-29 17:37:52 +0200

    Serve Highlight.js package assets

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { PUBLIC_DIR } = require("./config/paths");
+const path = require("path");
+const { NODE_MODULES_DIR, PUBLIC_DIR } = require("./config/paths");
 const toolsApiRoutes = require("./routes/api/tools");
 const markdownApiRoutes = require("./routes/api/markdown");
 const pageRoutes = require("./routes/pages");
@@ -10,6 +11,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(PUBLIC_DIR));
+app.use(
+  "/vendor/highlight.js",
+  express.static(path.join(NODE_MODULES_DIR, "highlight.js")),
+);
 
 app.use(toolsApiRoutes);
 app.use(markdownApiRoutes);
